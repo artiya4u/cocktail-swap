52631b6debd63d91bd3eaf9612f8648ee16bac9c
Artiya Thinkumpang <artiya4u@gmail.com>
2022-07-02 17:03:32 +0700

    Revert "Update DB"
    This reverts commit 844d50ddceb8a7aca129cfe9c122ca21cbdbf491.

--- swap.sql
+++ swap.sql
@@ -1,20 +1,22 @@
 create table swaps
 (
     txHash          varchar(66)     not null primary key,
-    valueUSD        decimal(48, 16) not null,
-    blockNumber     int             not null,
     swapAt          timestamp       not null,
-    tokenOutDecimal int             not null,
+    blockNumber     int             not null,
+    valueUSD        decimal(48, 16) not null,
     swapper         varchar(42)     not null,
     router          varchar(42)     not null,
+
     tokenOut        varchar(42)     not null,
     tokenOutName    varchar(42)     not null,
+    tokenOutDecimal int             not null,
     tokenOutSymbol  varchar(20)     not null,
     amountOut       varchar(80)     not null,
+
     tokenIn         varchar(42)     not null,
     tokenInName     varchar(42)     not null,
-    tokenInDecimal  int             not null,
     tokenInSymbol   varchar(20)     not null,
+    tokenInDecimal  int             not null,
     amountIn        varchar(80)     not null
 ) charset = latin1;
 
